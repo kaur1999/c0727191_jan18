f2263c6d352b6304347c07f8ccceb07a976e3fe2
visualstudio <visualstudio@122-PC31>
2019-01-18 21:50:54 -0500

    playing with arrays

--- c0727191/Program.cs
+++ c0727191/Program.cs
@@ -10,6 +10,101 @@ namespace c0727191
     {
         static void Main(string[] args)
         {
+            StudentInformation Mary = new StudentInformation("Mary", 3.8, 20);
+
+            StudentInformation John = new StudentInformation("John", 1.8, 2);
+
+            StudentInformation Mark = new StudentInformation("Mark", 2.5, 3);
+
+            StudentInformation Steve = new StudentInformation("Steve", 3.3, 18);
+
+            StudentInformation Cindy = new StudentInformation("Cindy", 2.3, 12);
+
+
+
+
+
+            var StudentList = new List<StudentInformation>();
+
+
+
+            StudentList.Add(Mary);
+
+            StudentList.Add(John);
+
+            StudentList.Add(Mark);
+
+            StudentList.Add(Steve);
+
+            StudentList.Add(Cindy);
+
+
+
+            foreach (var Student in StudentList)
+
+            {
+
+                Console.WriteLine(Student.Name);
+
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+
+    }
+
+
+
+    class StudentInformation
+
+    {
+
+        public string Name;
+
+
+
+        public double GPA;
+
+        public int Attn;
+
+
+
+        public StudentInformation(string aName, double aGPA, int aAttn)
+
+        {
+
+            Name = aName;
+
+            GPA = aGPA;
+
+            Attn = aAttn;
+
+        }
+
+
+
+    }
+
+
+
+    class StudentList
+
+    {
+
+        public void CICS()
+
+        {
+
         }
     }
 }
+
+
+
+
+
+
+
